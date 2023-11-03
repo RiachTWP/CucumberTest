d36e289751511ce970561658c6fa2fc634df0832
RiachTWP <139956318+RiachTWP@users.noreply.github.com>
2023-11-03 14:58:08 +0700

    Add files via upload

---
+++ src/test/java/com/riachs/baseTest.java
@@ -0,0 +1,17 @@
+package com.riachs;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class baseTest {
+    protected static WebDriver driver;
+
+    protected void starDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+   }
+}
